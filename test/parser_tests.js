fa2e689cebcf4065fffd5def8025c799d23ac5d3
Matt Gollob <mattgollob@gmail.com>
2013-05-12 15:55:10 -0400

    Add licence text to parser tests

--- test/parser_tests.js
+++ test/parser_tests.js
@@ -1,5 +1,24 @@
-var fs = require('fs');
-var path = require('path');
+// Copyright (c) Matt Gollob and other wow-analyze contributors.
+// All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 var stream = require('stream');
 var tap = require('tap');
 var Parser = require('../log-parser');
@@ -12,6 +31,8 @@ var data = [
 
 
 tap.test('parsing yields the correct number of records', function(t) {
+  var fs = require('fs');
+  var path = require('path');
   var p = new Parser();
   var recordCount = 0;
 
